37364c83c94f26759cbdec63f203f2ab8b06eb2b
kimanicharles911 <kimanicharles911@gmail.com>
2021-12-06 14:55:25 +0300

    ninth deploy to heroku

--- app.js
+++ app.js
@@ -156,9 +156,10 @@ app.delete('/api/repository', (req, res) => {
 }); */
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname, 'frontend/build')));
+  // app.use(express.static(path.join(__dirname, 'frontend/build')));
+  app.use(express.static('frontend/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
   });
 }
 
