67c17b0ff61e4bea4a76c14c82633a206dd366ec
kimanicharles911 <kimanicharles911@gmail.com>
2021-12-06 15:02:46 +0300

    twelfth deploy to heroku

--- app.js
+++ app.js
@@ -156,8 +156,8 @@ app.delete('/api/repository', (req, res) => {
 }); */
 
 if(process.env.NODE_ENV === 'production'){
-  // app.use(express.static(path.join(__dirname, 'frontend/build')));
-  app.use(express.static('frontend/build'));
+  app.use(express.static(path.join(__dirname, 'frontend/build')));
+  // app.use(express.static('frontend/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
   });
