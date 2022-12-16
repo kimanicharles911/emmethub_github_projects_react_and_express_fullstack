5a6550695f4ea2fe3e01119cedca35baa07dd632
kimanicharles911 <kimanicharles911@gmail.com>
2022-12-17 00:34:33 +0300

    correct cors config

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-import cors from 'cors';
-import corsOptions from './config/corsOptions.module.js';
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions.module.js');
 app.use(cors(corsOptions));
 const port = require('./config');
 app.use(express.json());
